7e6c40ad68e998369f294b53a82478730b06d80b
Vinay0022 <vinu031995@gmail.com>
2024-12-02 18:49:53 +0530

    First Configuration

---
+++ .emacs.custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
+ '(display-line-numbers 'relative)
+ '(inhibit-startup-screen t)
+ '(org-agenda-files '("/home/reji/org/tasks.org" "/home/reji/org/habits.org"))
+ '(package-selected-packages
+   '(exwm nov org-roam consult marginalia ivy-rich swiper doom-modeline evil-org org-bullets evil-escape evil orderless vertico modus-themes gruber-darker-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
