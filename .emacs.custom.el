0de6a6b0ef41f9e483438dbcc5facf91f90954a0
Vinay0022 <vinu031995@gmail.com>
2025-01-29 13:42:40 +0530

    changed the basic config after reinstall of linux

--- .emacs.custom.el
+++ .emacs.custom.el
@@ -3,16 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(gruber-darker))
- '(custom-safe-themes
-   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
- '(display-line-numbers 'relative)
- '(erc-modules
-   '(hl-nicks netsplit fill button match track completion readonly networks ring autojoin noncommands irccontrols move-to-prompt stamp menu list))
- '(inhibit-startup-screen t)
- '(org-agenda-files '("/home/reji/org/tasks.org" "/home/reji/org/habits.org"))
  '(package-selected-packages
-   '(exec-path-from-shell vterm mpv empv emms erc-sasl erc-hl-nicks lsp-mode pdf-tools eglot undo-tree org-roam-ui desktop-environment evil-collection magit exwm nov org-roam consult marginalia ivy-rich swiper doom-modeline evil-org org-bullets evil-escape evil orderless vertico modus-themes gruber-darker-theme)))
+   '(all-the-icons-nerd-fonts consult doom-modeline empv erc-hl-nicks
+			      evil-collection evil-escape evil-org
+			      exec-path-from-shell gruber-darker-theme
+			      lsp-mode magit marginalia mpv nov
+			      orderless org-bullets org-roam-ui
+			      pdf-tools undo-tree vertico vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,33 +1,46 @@
 (setq frame-resize-pixelwise t)
-		  (setq custom-file "~/.emacs.d/.emacs.custom.el")
-(load-theme 'gruber-darker t nil)
+ 		  (setq custom-file "~/.emacs.d/.emacs.custom.el")
+ (load-theme 'gruber-darker t nil)
 
-    ;;show battery, data and time 
-    (display-battery-mode 1)
-    (setq display-time-day-and-date 1)
-    (display-time-mode 1)
+     ;;show battery, data and time 
+     (display-battery-mode 1)
+     (setq display-time-day-and-date 1)
+     (display-time-mode 1)
+(setq inhibit-splash-screen nil) 
 
-    ;; The default is 800 kilobytes.  Measured in bytes.
-    (setq gc-cons-threshold (* 50 1000 1000))
+     ;; The default is 800 kilobytes.  Measured in bytes.
+     (setq gc-cons-threshold (* 50 1000 1000))
 
-	  (add-hook 'text-mode-hook 'visual-line-mode)
-		  (tool-bar-mode -1)
-		  (menu-bar-mode -1)
-		  (scroll-bar-mode -1)
-		  (global-display-line-numbers-mode)
-		  (load-file custom-file)
+ 	  (add-hook 'text-mode-hook 'visual-line-mode)
+ 		  (tool-bar-mode -1)
+ 		  (menu-bar-mode -1)
+ 		  (scroll-bar-mode -1)
+ 		  (global-display-line-numbers-mode)
+ 		  (load-file custom-file)
 
 
-	      (setq display-line-numbers-type 'relative)
-	    (setq backup-directory-alist '(("." . "~/.emacs.d/emacs_backup")))
+ 	      (setq display-line-numbers-type 'relative)
+ ;; backup and autosave file config
+ 	    (setq backup-directory-alist '(("." . "~/.emacs.d/emacs_backup")))
 
-	(dolist (mode `(shell-mode-hook
-	eshell-mode-hook
-	vterm-mode-hook
-      term-mode-hook))
-	(add-hook mode (lambda () (display-line-numbers-mode 0))))
+ (setq auto-save-file-name-transforms
+ `((".*" "~/.emacs.d/.emacs-saves/" t)))
+
+ 	(dolist (mode `(shell-mode-hook
+ 	eshell-mode-hook
+ 	vterm-mode-hook
+       term-mode-hook))
+ 	(add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (set-face-attribute 'default nil :family "Hack Bold" :height 150)
+(use-package nerd-icons
+  :ensure t
+  :custom
+  (nerd-icons-font-family "FiraCode Nerd Font")
+  ;; The Nerd Font you want to use in GUI
+  ;; "Symbols Nerd Font Mono" is the default and is recommended
+  ;; but you can use any other Nerd Font if you want
+  )
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -38,123 +51,123 @@
 (package-initialize)
 
 (use-package vertico
-		  :ensure t
+		    :ensure t
     :bind (:map vertico-map
      ("C-j" . vertico-next)
      ("C-k" . vertico-previous)
 )
-		  :custom 
-		  (vertico-cycle t)
-		  :init 
-		  (vertico-mode))
+		    :custom 
+		    (vertico-cycle t)
+		    :init 
+		    (vertico-mode))
 
-		  (use-package marginalia
-		  :after vertico
-		  :ensure t
-		  :custom
-		  (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
-		  :init
-		  (marginalia-mode))
-
-		    (use-package orderless
+		    (use-package marginalia
+		    :after vertico
 		    :ensure t
-		    :config
-		    (setq completion-styles '(orderless basic))
-		    (setq completion-category-defaults nil)
-		    (setq completion-category-overrride nil))
+		    :custom
+		    (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+		    :init
+		    (marginalia-mode))
 
-		    (use-package savehist
-		    :ensure nil ; it is built-in
-		    :hook (after-init . savehist-mode))
+		      (use-package orderless
+		      :ensure t
+		      :config
+		      (setq completion-styles '(orderless basic))
+		      (setq completion-category-defaults nil)
+		      (setq completion-category-overrride nil))
 
-		    (use-package consult
-		    :ensure t
-		    :bind (;; A recursive grep
-		    ("M-s M-g" . consult-grep)
-		    ;; Search for files names recursively
-		    ("M-s M-f" . consult-find)
-		    ;; Search through the outline (headings) of the file
-		    ("M-s M-o" . consult-outline)
-		    ;; Search the current buffer
-		    ("M-s M-l" . consult-line)
-		    ;; Switch to another buffer, or bookmarked file, or recently
-		    ;; opened file.
-		    ("M-s M-b" . consult-buffer)))
+		      (use-package savehist
+		      :ensure nil ; it is built-in
+		      :hook (after-init . savehist-mode))
 
-
-
-		    (use-package org-bullets
-		    :ensure t
-		    :hook
-		    (org-mode . org-bullets-mode))
-
-	(use-package org-roam
-	  :ensure t
-	  :custom
-	  (org-roam-directory (file-truename "~/org/org_notes/"))
-	  :bind (("C-c n l" . org-roam-buffer-toggle)
-		 ("C-c n f" . org-roam-node-find)
-		 ("C-c n g" . org-roam-graph)
-		 ("C-c n i" . org-roam-node-insert)
-		 ("C-c n c" . org-roam-capture))
-	  :config
-	  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-	  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-	  (org-roam-db-autosync-mode))
+		      (use-package consult
+		      :ensure t
+		      :bind (;; A recursive grep
+		      ("M-s M-g" . consult-grep)
+		      ;; Search for files names recursively
+		      ("M-s M-f" . consult-find)
+		      ;; Search through the outline (headings) of the file
+		      ("M-s M-o" . consult-outline)
+		      ;; Search the current buffer
+		      ("M-s M-l" . consult-line)
+		      ;; Switch to another buffer, or bookmarked file, or recently
+		      ;; opened file.
+		      ("M-s M-b" . consult-buffer)))
 
 
-		    (use-package org
-		    :ensure t
-		    :config
-		    (setq org-agenda-files
-		    '("~/org/tasks.org"
-		    "~/org/habits.org"
-		    "~/org/birthdays.org")
-		    org-hide-emphasis-markers t
-		    org-agenda-start-with-log-mode t
-		    org-log-done 'time
-		    org-log-into-drawer t)
 
-		(setq org-todo-keywords
-		  '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
-		    ))
+		      (use-package org-bullets
+		      :ensure t
+		      :hook
+		      (org-mode . org-bullets-mode))
 
-		;; Configure custom agenda views
-		(setq org-agenda-custom-commands
-		 '(("d" "Dashboard"
-		   ((agenda "" ((org-deadline-warning-days 7)))
-		    (todo "NEXT"
-		      ((org-agenda-overriding-header "Next Tasks")))
-		    ))
+	  (use-package org-roam
+	    :ensure t
+	    :custom
+	    (org-roam-directory (file-truename "~/org/org_notes/"))
+	    :bind (("C-c n l" . org-roam-buffer-toggle)
+		   ("C-c n f" . org-roam-node-find)
+		   ("C-c n g" . org-roam-graph)
+		   ("C-c n i" . org-roam-node-insert)
+		   ("C-c n c" . org-roam-capture))
+	    :config
+	    ;; If you're using a vertical completion framework, you might want a more informative completion interface
+	    (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+	    (org-roam-db-autosync-mode))
 
-		  ("n" "Next Tasks"
-		   ((todo "NEXT"
-		      ((org-agenda-overriding-header "Next Tasks")))))
-	    )))
+
+		      (use-package org
+		      :ensure t
+		      :config
+		      (setq org-agenda-files
+		      '("~/org/tasks.org"
+		      "~/org/habits.org"
+		      "~/org/birthdays.org")
+		      org-hide-emphasis-markers t
+		      org-agenda-start-with-log-mode t
+		      org-log-done 'time
+		      org-log-into-drawer t)
+
+		  (setq org-todo-keywords
+		    '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+		      ))
+
+		  ;; Configure custom agenda views
+		  (setq org-agenda-custom-commands
+		   '(("d" "Dashboard"
+		     ((agenda "" ((org-deadline-warning-days 7)))
+		      (todo "NEXT"
+			((org-agenda-overriding-header "Next Tasks")))
+		      ))
+
+		    ("n" "Next Tasks"
+		     ((todo "NEXT"
+			((org-agenda-overriding-header "Next Tasks")))))
+	      )))
 
     (use-package org-roam-ui
     :ensure t
-	:after org-roam
+	  :after org-roam
     ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
     ;;         a hookable mode anymore, you're advised to pick something yourself
     ;;         if you don't care about startup time, use
     ;;  :hook (after-init . org-roam-ui-mode)
-	:config
-	(setq org-roam-ui-sync-theme t
-	      org-roam-ui-follow t
-	      org-roam-ui-update-on-save t
-	      org-roam-ui-open-on-start t))
+	  :config
+	  (setq org-roam-ui-sync-theme t
+		org-roam-ui-follow t
+		org-roam-ui-update-on-save t
+		org-roam-ui-open-on-start t))
 
 
-      (setq org-clock-sound "~/me/sounds/bright-notifications-151766.wav") 
+	(setq org-clock-sound "~/me/sounds/bright-notifications-151766.wav") 
 
 
 
-       ;; Read ePub files
-	(use-package nov
-	  :ensure t
-	  :init
-	  (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode)))
+	 ;; Read ePub files
+	  (use-package nov
+	    :ensure t
+	    :init
+	    (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode)))
 
 (use-package magit
 :ensure t)
@@ -209,15 +222,15 @@
   :ensure t)
 
 (dolist (mode `(c-mode-hook
-		java-mode-hook
-		javascript-mode-hook
-		c++-mode-hook
-		typescript-ts-mode-hook))
+		  java-mode-hook
+		  javascript-mode-hook
+		  c++-mode-hook
+		  typescript-ts-mode-hook))
   (add-hook mode (lambda () ('lsp))))
 
 (use-package doom-modeline
-		  :ensure t
-		  :init
+		    :ensure t
+		    :init
 (doom-modeline-mode 1))
 
 (add-hook 'erc-mode-hook (lambda () (display-line-numbers-mode 0)))
@@ -234,17 +247,17 @@
 (require 'erc)
 (require 'erc-sasl)
     (setq erc-sasl-server-regexp-list '(".*") ;; Use SASL for all servers
-	  erc-sasl-use-sasl t)
+	    erc-sasl-use-sasl t)
 
   ;; Use auth-source for credentials
   (require 'auth-source)
   (setq auth-sources '("~/.authinfo.gpg" "~/.authinfo" "~/.netrc")
-	erc-sasl-user (lambda () (plist-get (car (auth-source-search :host "irc.libera.chat")) :user))
-	erc-sasl-password (lambda () (plist-get (car (auth-source-search :host "irc.libera.chat")) :secret)))
+	  erc-sasl-user (lambda () (plist-get (car (auth-source-search :host "irc.libera.chat")) :user))
+	  erc-sasl-password (lambda () (plist-get (car (auth-source-search :host "irc.libera.chat")) :secret)))
 
   ;; Autojoin specific channels
   (setq erc-autojoin-channels-alist
-	'(("Libera.Chat" "##programming")))
+	  '(("Libera.Chat" "##programming")))
 
 ;; Start ERC with TLS
 (defun efs/erc-libera-chat ()
@@ -257,12 +270,12 @@
   :config
 (setq empv-audio-dir "~/music"))
 
-	(use-package mpv
-      :ensure t)
+	  (use-package mpv
+	:ensure t)
 
 (use-package vterm
-      :ensure t)
-      (use-package exec-path-from-shell
+	:ensure t)
+	(use-package exec-path-from-shell
     :ensure t
   :config
 (exec-path-from-shell-initialize))
